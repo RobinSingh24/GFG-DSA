7646caa84e2f6d637eb69ec2864c7c47cec0164d
Robin SIngh <robin.122497@gmail.com>
2023-03-18 19:22:46 +0530

    Updated CountDigits class

--- src/Mathematics/CountDigits.java
+++ src/Mathematics/CountDigits.java
@@ -2,14 +2,17 @@ package Mathematics;
 
 public class CountDigits {
 
-    public static void main(String [] args){
-        int n = 123;
+    public static int countDigits(int n){
         int count=0;
         while(n!=0){
             n/=10;
             ++count;
         }
-        System.out.println(count);
+        return count;
+    }
+
+    public static void main(String [] args){
+        System.out.println(countDigits(123));
     }
 
 }
